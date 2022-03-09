aaead0fc48ee003002d3c24f681332fdb549caca
KimSooYoung <55309351+kimdlzp@users.noreply.github.com>
2022-03-09 18:55:23 +0900

    fix signin

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,14 +1,63 @@
-import { createSalt, createSaltePassword } from "./HashFunctions";
+require("dotenv").config();
+const { createSaltedPassword } = require("./HashFunctions");
 const models = require("./../models");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
   localSignIn: async (req, res) => {
-    const { email, password } = req.body;
     const userInfo = await models.User.findOne({
-      where: { email: email, password: password },
+      where: { email: req.body.email },
+    }).catch((err) => {
+      return res.status(500).json({ code: 500, error: err });
     });
 
-    res.json({ code: 200 });
+    // DB에 없는 이메일인 경우
+    if (!userInfo) {
+      return res.status(404).json({ code: 404, error: "not found" });
+    }
+
+    const {
+      userName,
+      email,
+      phoneNum,
+      role,
+      oauthLogin,
+      createdAt,
+      updatedAt,
+      oauthCI,
+    } = userInfo;
+
+    const { saltedPassword } = await createSaltedPassword(
+      req.body.password,
+      userInfo.salt
+    ).catch((err) => {
+      return res.status(500).json({ code: 500, error: err });
+    });
+
+    if (saltedPassword === userInfo.saltedPassword) {
+      const accessToken = jwt.sign(
+        {
+          userName,
+          email,
+          phoneNum,
+          role,
+          oauthLogin,
+          createdAt,
+          updatedAt,
+          oauthCI,
+        },
+        process.env.ACCESS_SECRET,
+        { expiresIn: "5h" }
+      );
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: true,
+      });
+      res.status(200).json({ code: 200, role: userInfo.role });
+    } else {
+      res.status(401).json({ code: 401, error: "unauthorized" });
+    }
   },
 };
